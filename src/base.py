4b4805949254fdfee5c498b8b54c74c30b9d9d9b
Gavin <gavin@Gavins-MacBook-Pro.local>
2021-02-09 12:47:30 +0000

    undo uses y

--- src/base.py
+++ src/base.py
@@ -64,3 +64,7 @@ class SupervisedModel(BaseModel):
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         super().fit(X)
         self._y = y
+
+    def predict(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+    
